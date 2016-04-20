fc4057c698f9d771798fce7eeab93f3242d17358
Sagar Arora <supriyaarora@1x-nat-vl931-172-30-139-166.wireless.umass.edu>
2016-04-20 01:18:42 -0400

    Add Image Remover Files

--- ImageRemover/ViewController.swift
+++ ImageRemover/ViewController.swift
@@ -4,12 +4,16 @@
 //
 //  Created by Sagar Arora  on 4/14/16.
 //  Copyright © 2016 Sagar Arora . All rights reserved.
-//
+/* Remember for buttons you dont need to link them to a outlet, just images and other things also dont need to include uneccesary boolean statements in functions.
+*/
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func hideRedBombButton(sender: AnyObject) {
+        redBomb.hidden = true
+    }
+   
+    @IBAction func hideBlueBombButton(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
 
 }
 
